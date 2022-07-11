150236b8d547e82c796bc9023c839ad98fa5ac79
Miguel de Icaza <miguel@gnome.org>
2022-07-11 16:33:38 -0400

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Miguel de Icaza on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
